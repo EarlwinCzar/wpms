275edbfdfa85cd707cdc22187eb14677c1055993
Earlwin Czar Cantillo <140438256+EarlwinCzar@users.noreply.github.com>
2024-06-28 17:13:07 +0900

    dot env added

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,12 @@ import firebase from "firebase/compat/app"; // Update import statement
 import "firebase/compat/firestore"; // Import Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6yHRbtkjiewE8zwlkbtqVXI6f65GmhJw",
-  authDomain: "wpms-53b67.firebaseapp.com",
-  projectId: "wpms-53b67",
-  storageBucket: "wpms-53b67.appspot.com",
-  messagingSenderId: "111383567669",
-  appId: "1:111383567669:web:49a77e091022fd6064c07c",
-  measurementId: "G-2KLBFNPZVG",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
